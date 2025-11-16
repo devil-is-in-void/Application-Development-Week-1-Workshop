f0a72821e50db79c4515858ae45bdc487ea16c63
devil-is-in-void <prajwalchaulagain35@gmail.com>
2025-11-16 10:37:22 +0545

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PersonalDetailsAppWeek1Workshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Personal Details ===");
+            Console.WriteLine("Name: Prajwal Chaulagain");
+            Console.WriteLine("Address: Biratnagar, Nepal");
+            Console.WriteLine("Phone Number: +977-9707156094");
+            Console.WriteLine("College: (Itahari International College)");
+
+            Console.WriteLine();
+
+            //call quote class
+
+            Quote quote_1 = new Quote();
+            quote_1.PrintQuote();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalDetailsAppWeek1Workshop
+{
+    internal class Quote
+    {
+        private string quote;
+        public Quote()
+        {
+            quote = "The best way to predict the future is to create it. - Peter Drucker";
+        }
+
+        public void PrintQuote()
+        {
+            Console.WriteLine("=== Inspirational Quote ===");
+            Console.WriteLine(quote);
+        }
+    }
+}
